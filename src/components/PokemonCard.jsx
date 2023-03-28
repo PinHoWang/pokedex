6e518b622c6873606a47d74cf6cad893549d1699
PinHoWang <abc8755987@gmail.com>
2023-03-28 14:08:36 +0800

    fix: images url for production

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,7 +5,7 @@ export default function PokemonCard({ name, picture, types }) {
     return (
         <div className='pokemon-card' data-testid='pokemon-card'>
             <h2>{name}</h2>
-            <img src={`/images/${picture}`} alt={picture} width="100" height="100" />
+            <img src={`images/${picture}`} alt={picture} width="100" height="100" />
             <div className='badges-row'>
             {
                 types.map((type, index) => (
